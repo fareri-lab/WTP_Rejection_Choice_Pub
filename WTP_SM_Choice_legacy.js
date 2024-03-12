46d06a320d4ed9a3189cd4f59f7ef8b9e1c5f86d
Dominic Fareri <dfareri@adelphi.edu>
2024-03-12 01:53:42 +0000

    Update WTP_SM_Choice_legacy.js

--- WTP_SM_Choice_legacy.js
+++ WTP_SM_Choice_legacy.js
@@ -260,7 +260,7 @@ async function experimentInit() {
     units: undefined, 
     pos: [0, 0], height: 0.08,  wrapWidth: undefined, ori: 0.0,
     languageStyle: 'LTR',
-    color: new util.Color('yellow'),  opacity: undefined,
+    color: new util.Color('pink'),  opacity: undefined,
     depth: 0.0 
   });
   
