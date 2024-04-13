5763065ee745b8fd94b747c19c0452304705e1e0
melanieziur <72086257+melanieziur@users.noreply.github.com>
2024-04-13 02:39:55 -0400

    with wtp trials

--- Rej-WTP_Trials.py
+++ Rej-WTP_Trials.py
@@ -6,42 +6,84 @@ Created on Tue Dec 12 21:27:39 2023
 @author: melanieruiz
 """
 
-import pandas as pd
 import os
+import pandas as pd
 from itertools import product
 
+completedparticipantlist = pd.read_excel('participantlist.xlsx')
+completedparticipantlist = completedparticipantlist.loc[
+    completedparticipantlist['PhotosUploaded? (y/n)'] == 'n']
+
+exp = pd.read_csv('WTP-Rej_Experiences.csv')
+
+clean = exp.filter(regex='ocial_A')
+clean.insert(0, 'PROLIFIC_PID', exp['PROLIFIC_PID'])
+
+path = os.getcwd()
+# parent = Path(path).parents[1]
+
+# sub_data = clean[clean["dem_ID"].str.contains(sub)]
 
 
-sub = 106
-# os.chdir('/Users/dfareri/Documents/GitHub/r15-WTP/stimuli/Scan-Thinking/event-related/params/sub-9999')
-wtp = pd.read_csv('/Users/dfareri/Dropbox/Dominic/Github/fareri-lab/r15-WTP/WTP_exp.csv')
 
-# wtp = pd.DataFrame(data=wtp1.append(wtp2))
+#%%
+template = pd.read_excel('WTP_trials_template.xlsx')
 
-
-# wtp['Left_Exp']= wtp1['Left_Exp']
-# wtp['Right_Exp']= wtp1['Right_Exp']
-
-new_wtp = pd.DataFrame(list(product(wtp['exp1'], wtp['exp2'])), columns=['Right', 'Left'])
-new_wtp['TrialNumber']= range(1,101)
-
-path= '/Users/dfareri/Dropbox/Dominic/Github/fareri-lab/r15-WTP/stimuli/Scan-WTP/event-related/params/'
-os.chdir(path)
-subdir = path + '/sub-9998/'
-os.makedirs(subdir, exist_ok=True)
-print('Directory for subject %s has been created' %(sub))
-
-run1 = new_wtp[(new_wtp["TrialNumber"] < 26)]
-run1.to_csv(subdir+ 'sub-9998_run-1_design.csv')
-
-run2 = new_wtp[(new_wtp["TrialNumber"] > 25) & (new_wtp["TrialNumber"] < 51)]
-run2.to_csv(subdir+'sub-9998_run-2_design.csv')
-
-run3 = new_wtp[(new_wtp["TrialNumber"] > 50) & (new_wtp["TrialNumber"] < 76)]
-run3.to_csv(subdir+ 'sub-9998_run-3_design.csv')
-
-
-run4 = new_wtp[(new_wtp["TrialNumber"] > 75) & (new_wtp["TrialNumber"] < 101)]
-run4.to_csv(subdir+ 'sub-9998_run-4_design.csv')
-
-# new_wtp.to_csv('')
+sub_folders= path + '/Participant_Images/'
+for sub in os.listdir(sub_folders):
+    if not sub.endswith('.DS_Store'): 
+        if not sub.startswith('101'): #we do not want DS store or blank file
+            sub_exp = clean.loc[
+            clean['PROLIFIC_PID'] == sub]
+            all_exp= pd.DataFrame()
+            sub_path= sub_folders + sub
+            #get nonsocial activities
+            nonsocial = sub_exp.filter(regex='Nonsocial_A')
+            nonsocial=nonsocial.T.reset_index()
+            nonsocial= nonsocial.drop(['index'], axis=1)
+            nonsocial.columns.values[0] = "nonsocial"
+    
+            # nonsocial= nonsocial.rename(columns={5: "nonsocial"}) #change column number
+            nonsocial['type'] =0
+            all_exp['nonsocial'] = nonsocial['nonsocial']
+            # nonsocial.to_csv('%s/%s_nonsocial.csv'%(subdir,sub), index= False)
+            # nonsocial=nonsocial.loc[nonsocial.index.repeat(10)]
+            # nonsocial.insert(0, 'trial', range(1,len(nonsocial)+1))
+            # nonsocial.insert(1, 'exp_type', nonsocial.rename('NS_{}'.format).index)
+            # nonsocial['nonsocial_left'] = np.where(nonsocial.trial % 2, 1, 0)
+            # nonsocial.columns.values[0] = "experience"
+    
+    
+    
+            #get social activities
+            social = sub_exp.filter(regex='Social_A')
+            social=social.T.reset_index()
+            social= social.drop(['index'], axis=1)
+            # social= social.rename(columns={5: "social"}) #change column number
+            social.columns.values[0] = "social"
+            social['type'] =1
+            
+            all_exp['social'] = social['social']
+    
+            # social.to_csv('%s/%s_social.csv' %(subdir, sub), index= False)
+            # social=social.loc[social.index.repeat(10)]
+            # social.insert(0, 'trial', range(1,len(social)+1))
+            # social.insert(1, 'exp_type', social.rename('S_{}'.format).index)
+            # social['social_left'] = np.where(social.trial % 2, 1, 0)
+            # social.columns.values[0] = "experience"
+            
+            # all_exp = pd.concat([nonsocial, social]).sort_index(kind='merge').reset_index()
+            # del all_exp[all_exp.columns[0]]
+            
+            # all_exp.to_csv('%s/%s_all_exp.csv' %(subdir,sub), index= False)
+            wtp = pd.DataFrame(list(product(all_exp['nonsocial'], all_exp['social'])), columns=['right', 'left'])
+            wtp = wtp.sample(frac = 1).reset_index(drop=True)
+            wtp.insert(0,'TrialNumber',range(1,101))
+            wtp.insert(3, 'leftmoney', template['LeftPrice'])
+            wtp.insert(4, 'rightmoney', template['RightPrice'])
+            wtp.insert(5,'WTP_ITI', template['ITI'])
+            wtp.to_csv(sub_path + '/%s_WTP.csv' %(sub), index=False)
+    
+    
+    
+    # new_wtp.to_csv('')
